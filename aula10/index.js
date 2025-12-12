7741922bf78d42d148fd9bd11ee9f1856e494715
Felipe Bolduan <flpbolduan@gmail.com>
2025-12-12 06:27:39 -0300

    Tipos de Dados Primitivos

---
+++ aula10/index.js
@@ -0,0 +1,15 @@
+/* Tipos de dados primitivos -> String, number, undefined, null, boolean e symbol*/
+
+const nome = 'Felipe'; // String
+const idade = 23; // Number
+const altura = 1.91; // Number
+let nomeAluno; // Undefined = não aponta para lugar nenhum na memória;
+let sobrenomeAluno = null; // Nulo = não aponta para local nenhum na memória;
+const boolean = true; // Valor Booleano (Verdadeiro ou Falso);
+
+console.log (typeof nome);
+console.log (typeof idade, idade); // Mostra o tipo e valor
+console.log (typeof altura);
+console.log (typeof nomeAluno);
+console.log (typeof sobrenomeAluno);
+console.log (typeof boolean);
---
+++ aula9/index.js
@@ -0,0 +1,8 @@
+//ECMAScript (ES) -> Padronização do JS
+
+var nome = 'Luiz';
+var nome = 'Otávio';
+
+console.log (nome);
+
+/* Permite redeclaração de variável, diferente do LET */
