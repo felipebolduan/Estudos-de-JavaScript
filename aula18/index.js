28c5155a82be9d9864b272bba32d884188b33172
Felipe Bolduan <flpbolduan@gmail.com>
2026-02-09 20:33:09 -0300

    Objeto Math

---
+++ aula18/index.js
@@ -0,0 +1,19 @@
+// Testando o Objeto Math
+
+let num1 = 9.453443;
+let num2 = Math.floor(num1); // Arredonda para baixo
+let num3 = Math.ceil(num1); // Arredonda para cima
+let num4 = Math.round(num1); // Arredonda para o número mais próximo
+let valor_pi = Math.PI;
+let num5 = 9;
+
+
+console.log (num2);
+console.log (num3);
+console.log (num4);
+console.log (Math.max(1,2,3,4,-5,10)); // Retorna o maior número de uma sequência
+console.log (Math.min(1,2,3,4,-5,10)); // Retorna o menor número de uma sequência
+console.log (Math.random()); // Seta um número aleatório
+console.log (valor_pi); // Exibe o valor de PI
+console.log (Math.pow(2,2)); // Eleva um número
+console.log (num5 ** (1/2)); // Raiz quadrada
